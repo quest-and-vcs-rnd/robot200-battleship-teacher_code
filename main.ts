b56a0d1dc52d6b716c791d18c303ebcc7aa7732d
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-03-26 17:11:02 -0700

    robot200-Battleship-TeacherCode (25-0326-1710, v0.2.0) *** Standardize 'on start' Stack

--- main.ts
+++ main.ts
@@ -1,16 +1,3 @@
-function setup_Staff_ThisLessonOnly_Func () {
-	
-}
-function setup_System_AnyLessonAlways_Func () {
-    quest_Dashboard.quest_Send_LoginOfBot_ToXrayDashboardOfMb_Func(
-    0,
-    quest_Toggle_OnOff_Enum.On,
-    quest_Debug_Show_Enum.Dashboard_OLED
-    )
-}
-function setup_Student_ThisLessonOnly_Func () {
-	
-}
 input.onButtonPressed(Button.A, function () {
     quest_Note_1.quest_Show_String_For_Note_Small_Func(
     "set variable \"number\" to count up by 1"
@@ -329,13 +316,6 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     "move right the number of spaces corresponding to the number of times button B was pressed"
     )
 })
-function pre_setup_System_AnyLessonAlways_Func () {
-    quest_Note_1.quest_Show_String_For_Note_Small_Func(
-    "Fix 'show leds' Block's Orientation for Driving"
-    )
-    display.rotateTo(display.Direction.UpsideDown)
-}
-pre_setup_System_AnyLessonAlways_Func()
 quest_Note_1.quest_Show_String_For_Note_Small_Func(
 "2-Sequence Animation Validates New-Start"
 )
@@ -343,13 +323,18 @@ basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-setup_Staff_ThisLessonOnly_Func()
-setup_System_AnyLessonAlways_Func()
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
+"Setup Code for Teacher Below:"
+)
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"micro:bit LED Display is upside-down on bot:"
+)
+display.rotateTo(display.Direction.UpsideDown)
 quest_Note_1.quest_Show_String_For_Note_Big_Func(
 "©️ 2025 Quest Institute. All rights reserved."
 )
-quest_Note_2.quest_Show_String_For_Note_Big_Func(
-"Student Setup-Code Can Start Below.."
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
+"Setup Code for Student Below:"
 )
 quest_Note_1.quest_Show_String_For_Note_Small_Func(
 "set letter variable to zero"
